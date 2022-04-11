35486fced1e82943a63bdfa8ab82cca7bb5965d1
Makoto <makoto@emurasoft.com>
2022-04-10 19:39:04 -0700

    palindromeDoubleLinked

--- 2.6_test.go
+++ 2.6_test.go
@@ -5,17 +5,6 @@ import (
 	"testing"
 )
 
-func getEnd(head *Node) *Node {
-	curr := head
-	for {
-		curr = curr.next
-
-		if curr.next == nil {
-			return curr
-		}
-	}
-}
-
 func listToArray(head *Node) []int {
 	if head == nil {
 		return nil
@@ -39,6 +28,57 @@ func palindromeSingleLinked(head *Node) bool {
 		if items[i] != items[len(items)-1-i] {
 			return false
 		}
+	}
+	return true
+}
+
+type DLNode struct {
+	next *DLNode
+	prev *DLNode
+	item int
+}
+
+func newDoubleLinkedList(items []int) *DLNode {
+	if len(items) == 0 {
+		return nil
+	}
+
+	head := &DLNode{}
+	curr := head
+
+	for _, item := range items {
+		curr.item = item
+		curr.next = &DLNode{prev: curr}
+		curr = curr.next
+	}
+
+	curr.prev.next = nil
+	return head
+}
+
+func getEnd(head *DLNode) *DLNode {
+	curr := head
+	for curr.next != nil {
+		curr = curr.next
+	}
+	return curr
+}
+
+// palindromeDoubleLinked returns true if the linked list is a palindrome.
+func palindromeDoubleLinked(head *DLNode) bool {
+	if head == nil {
+		return true
+	}
+
+	start := head
+	end := getEnd(head)
+
+	for start != end {
+		if start.item != end.item {
+			return false
+		}
+		start = start.next
+		end = end.prev
 	}
 	return true
 }
@@ -140,9 +180,80 @@ func TestPalindromeSingleLinked(t *testing.T) {
 			},
 			true,
 		},
+		{
+			&Node{
+				&Node{
+					&Node{
+						&Node{
+							&Node{
+								nil,
+								0,
+							},
+							2,
+						},
+						2,
+					},
+					1,
+				},
+				0,
+			},
+			false,
+		},
 	}
 
 	for i, test := range tests {
 		assert.Equal(t, test.expected, palindromeSingleLinked(test.head), i)
+	}
+}
+
+func TestPalindromeDoubleLinked(t *testing.T) {
+	tests := []struct {
+		head     *DLNode
+		expected bool
+	}{
+		{
+			nil,
+			true,
+		},
+		{
+			newDoubleLinkedList([]int{0}),
+			true,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1}),
+			false,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1, 0}),
+			true,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1, 2}),
+			false,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1, 1, 0}),
+			true,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1, 2, 0}),
+			false,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1, 2, 1, 0}),
+			true,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1, 2, 2, 1, 0}),
+			true,
+		},
+		{
+			newDoubleLinkedList([]int{0, 1, 2, 2, 3, 0}),
+			false,
+		},
+	}
+
+	for i, test := range tests {
+		assert.Equal(t, test.expected, palindromeDoubleLinked(test.head), i)
 	}
 }
